bcc7db708ba8cd31dc30267aa646248b7f2e3d75
Bor333 <sajblerboris@gmail.com>
2024-08-09 12:48:01 +0700

    add message done

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 include 'db.php';
 $mysqli = getMysqli();
-
 $result = $mysqli->query("SELECT * FROM `messages`");
+
+if (isset($_GET['action']) && ($_GET['action'] == 'add')) {
+    $heading = $_GET['heading'];
+    $author = $_GET['author'];
+    $preview = $_GET['preview'];
+    $text = $_GET['text'];
+    $sql = "INSERT INTO `messages`(`heading`, `author`, `preview`, `text`) VALUES ('{$heading}','{$author}','{$preview}','{$text}')";
+    $mysqli->query($sql);
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -15,14 +23,22 @@ $result = $mysqli->query("SELECT * FROM `messages`");
 </head>
 <body>
 <ul>
-   <?php foreach ($result as $message): ?>
-   <li style="list-style-type: none">
-      <h3> <?= $message['heading'] ?></h3>
-      <p>Автор: <?= $message['author'] ?></p>
-      <a href="/message.php?id=<?= $message['id'] ?>"><?= $message['preview'] ?></a>
-   </li>
+    <?php foreach ($result as $message): ?>
+        <li style="list-style-type: none">
+            <h3> <?= $message['heading'] ?></h3>
+            <p>Автор: <?= $message['author'] ?></p>
+            <a href="/message.php?id=<?= $message['id'] ?>"><?= $message['preview'] ?></a>
+        </li>
     <?php endforeach; ?>
 </ul>
-
+<h4>Добавить комментарий</h4>
+<form>
+    <input hidden type="text" name="action" value="add">
+    <p><input type="text" name="heading" required> заголовок</p>
+    <p><input type="text" name="author" required> автор</p>
+    <p><input type="text" name="preview" required> краткое содержание</p>
+    <p><input type="text" name="text" required> полное содержание</p>
+    <input type="submit">
+</form>
 </body>
 </html>
